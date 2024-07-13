7dd8a917b97bccab707af01e810b5977722c0a38
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2024-07-13 13:04:44 +0530

    get

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express= require('express');
-const app= express();
-const db= require('./db')
+const express = require("express");
+const app = express();
+const db = require("./db");
 
-const bodyParser= require('body-parser'); // data ko confre krta h jo jaruri ho format convert keke bhejta h
-app.use(bodyParser.json())
-const Person= require('./models/Person')
+const bodyParser = require("body-parser"); // data ko confre krta h jo jaruri ho format convert keke bhejta h
+app.use(bodyParser.json());
+const Person = require("./models/Person");
 
-app.get('/',function(req,res){
-res.send("welcome to my hotel how may i help you")
-})
+app.get("/", function (req, res) {
+  res.send("welcome to my hotel how may i help you");
+});
 // create a new person doc usingg mongoose model
 // app.post('/person',(req,res)=>{
 // const data= req.body;
@@ -34,39 +34,34 @@ res.send("welcome to my hotel how may i help you")
 // //     }
 // // })
 
-
-
-
-
-
-
-
 // })
 
+app.post("/person", async (req, res) => {
+  try {
+    const data = req.body;
+    const newPerson = new Person(data);
 
+    const response = await newPerson.save();
+    console.log("data sved");
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Interal Server Error" });
+  }
+});
 
+//get metod
+app.get("/person", async (req, res) => {
+  try {
+    const data = await Person.find();
+    console.log("data find");
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server eerror" });
+  }
+});
 
-
-app.post('/person',async (req,res)=>{
-  
-    try{
-const data= req.body;
-const newPerson= new Person(data);
-
-const response= await newPerson.save();
-console.log('data sved');
-res.status(200).json(response);
-    }
-    catch(err){
-console.log(err);
-res.status(500).json({error:'Interal Server Error'})
-    }
-    
-    
-    
-
-    })
-
-app.listen(3000,()=>{
-    console.log('listing on port 3000');
-})
+app.listen(3000, () => {
+  console.log("listing on port 3000");
+});
