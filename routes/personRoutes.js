300a12324b2811ab30763ac0b478d3d069e5e31c
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2024-07-16 23:08:27 +0530

    delete_put

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -50,4 +50,47 @@ res.status(500).json({error:'Internal Server error'})
    }
   })
 
+  // update
+router.put('/:id',async (req,res)=>{
+try{
+const personId=req.params.id;
+const updatedPersonData= req.body;
+
+const response= await Person.findByIdAndUpdate(personId,updatedPersonData,{
+  new:true, // return updated document
+  runValidators:true //run mongose validation
+})
+if(!response){
+  return res.status(404).json({error:'Person not found'});
+
+}
+console.log('data updated');
+res.status(200).json(response);
+
+
+}
+catch(err){
+console.log(err);
+res.status(500).json({error:'Internal Server Error'})
+}
+
+})
+
+//delete
+router.delete('/:id',async (req,res)=>{
+  try{
+const personId= req.params.id;
+const response = await Person.findByIdAndDelete(personId);
+if(!response){
+  return res.status(404).json({error:'Person not found'})
+}
+console.log('data deleted');
+res.status(200).json({message:"person Deleted Successfully"})
+  }
+  catch(err){
+console.log(err);
+res.status(500).json({error:"Internal Server Error"})
+  }
+})
+
   module.exports= router;
